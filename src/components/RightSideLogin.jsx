46cadfa65b7bdf6c04f634d41c4a9072b1e163c4
Ramon-UFSJ2023 <ramonsjdr70@aluno.ufsj.edu.br>
2025-09-25 10:58:12 -0300

    Começando tela de Login

---
+++ src/components/RightSideLogin.jsx
@@ -0,0 +1,23 @@
+import "../styles/login.css"
+
+export default function RightSideLogin(){
+    return(
+      <aside className="painel-Right">
+        <div className="content">
+          <div className="box-text-right">
+            <h2 className="title-right" id="title-login">
+              Paciente
+            </h2>
+          </div>
+          <span className="">
+            <h3 className=""></h3>
+          </span>
+          
+        </div>
+
+        <footer className="bottom">
+          <p>@Copyright2026</p>
+        </footer>
+      </aside>
+    );
+}
