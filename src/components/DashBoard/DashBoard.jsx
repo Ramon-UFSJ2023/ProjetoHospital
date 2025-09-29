7514a580eb55f75260359951cca7a0cdc39bb633
Ramon-UFSJ2023 <ramonsjdr70@aluno.ufsj.edu.br>
2025-09-29 20:19:09 -0300

    Criando o dashboardPaciente

---
+++ src/components/DashBoard/DashBoard.jsx
@@ -0,0 +1,10 @@
+import "./dashboard.css"
+
+
+export default function DashBoard() {
+  return (
+
+  <div class="background-dashboard">
+  </div>
+  );
+}
--- src/pages/Paciente/DashBoardPaciente.jsx
+++ src/pages/Paciente/DashBoardPaciente.jsx
@@ -1,12 +1,14 @@
-import "./stylePaciente/dashBoard.css"
+import "./stylePaciente/dashBoardPaciente.css";
 import React from "react";
 import UpperBar from "../../components/UpperBar/UpperBar";
+import DashBoard from "../../components/DashBoard/DashBoard";
 import { functionsPacienteNavBar } from "../../config/itemsSecondNavBar";
 
-
-export default function DashBoardPaciente(){
-
-    return(
-        <UpperBar items = {functionsPacienteNavBar}/>
-    );
+export default function DashBoardPaciente() {
+  return (
+    <div>
+      <UpperBar items={functionsPacienteNavBar} />
+      <DashBoard />
+    </div>
+  );
 }
