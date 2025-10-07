2cc0543913e640fd52a7cadc6c23f7680fd19572
Ramon-UFSJ2023 <ramonsjdr70@aluno.ufsj.edu.br>
2025-10-07 11:08:36 -0300

    Tela Cadastro
    comecei a criação e organização da tela de cadastro

---
+++ src/components/PageCadastro/PageCadastro.jsx
@@ -0,0 +1,9 @@
+
+
+export default function PageCadastro(){
+    return (
+        <div className="Container">
+            
+        </div>
+    );
+}
---
+++ src/pages/Paciente/PacienteCadastro.jsx
@@ -0,0 +1,11 @@
+import "./stylePaciente/cadastro-paciente-style.css"
+import LeftSide from "../../components/LeftSide";
+
+export default function PacienteCadastro(){
+    return(
+        <div className="Container">
+            <LeftSide />
+            
+        </div>
+    );
+}
